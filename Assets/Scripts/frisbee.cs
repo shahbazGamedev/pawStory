5feea9e344f707188ba61276881eaba234ca38c6
Srivatsan <srivatsan@skytousolution.com>
2015-06-11 16:02:31 +0530

    frisbee

---
+++ Assets/Scripts/frisbee.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class frisbee : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
