f44b657c64c71ae9c520dab2e2d6a868e8ceb6b4
Srivatsan <srivatsan@skytousolution.com>
2015-06-15 12:19:08 +0530

    frisbee speed issue

--- Assets/Scripts/FrisbeeMovement.cs
+++ Assets/Scripts/FrisbeeMovement.cs
@@ -1,26 +1,11 @@
-﻿
-/**
-Script Author : Srivatsan 
-Description   : Dog Frisbee movement
-**/
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
-<<<<<<< HEAD
 
 public class FrisbeeMovement : MonoBehaviour {
-=======
-using UnityEngine.EventSystems;
-
-
-public class FrisbeeMovement : MonoBehaviour 
-{
->>>>>>> origin/master
 	
 	
 	private float power= 500.0f;
 	Rigidbody rb;
-<<<<<<< HEAD
 	public Vector3 endPos;
 	public Vector3 force;
 	
@@ -47,47 +32,6 @@ public class FrisbeeMovement : MonoBehaviour
 		
 		rb.AddForce(force * power);
 		//ReturnBall();
-=======
-	public float curveAmount;
-	public float height;
-
-	// Use this for initialization
-	void Start () 
-	{
-		rb = GetComponent<Rigidbody> ();
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () 
-	{
-		Vector3 sideDir = Vector3.Cross(transform.up, rb.velocity).normalized;
-		rb.AddForce(sideDir * curveAmount);
-	}
-
-	public void OnPointerDown(BaseEventData  data)
-	{
-		Debug.Log("Begins");
-		PointerEventData e=(PointerEventData) data;
-		swipeBegin=e.position;
-	}
-	
-	public void OnPointerUp(BaseEventData  data)
-	{
-		Debug.Log("Ends");
-		PointerEventData e=(PointerEventData) data;
-		swipeEnd=e.position;
-		DetectSwipe();
-	}
-
-	void DetectSwipe()
-	{
-		Debug.Log ("swiped");
-		Vector2 direction = swipeEnd - swipeBegin;
-		Vector3 newDirection = new Vector3 (direction.x, height, direction.y);
-		Vector3 velocity = newDirection * speed;
-		rb.velocity = velocity;
->>>>>>> origin/master
 	}
 	
 	// void ReturnBall (){
