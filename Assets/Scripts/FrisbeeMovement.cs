41606600d1457e55c3004a42ab2d5b594ba5ca64
Srivatsan <srivatsan@skytousolution.com>
2015-06-15 12:10:13 +0530

    frisbee speed changes

--- Assets/Scripts/FrisbeeMovement.cs
+++ Assets/Scripts/FrisbeeMovement.cs
@@ -1,45 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.EventSystems;
+
 public class FrisbeeMovement : MonoBehaviour {
 	
-	Vector2 swipeBegin;		
-	Vector2 swipeEnd;
-	public  float speed;
+	
+	private float power= 500.0f;
 	Rigidbody rb;
-	public float curveAmount;
-	public float height;
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody> ();
+	public Vector3 endPos;
+	public Vector3 force;
+	
+	private Vector3 startPos;
+	void  Start (){
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	
+	void  OnMouseDown (){
+		startPos = Input.mousePosition;
+		startPos.z = transform.position.z - Camera.main.transform.position.z;
+		startPos = Camera.main.ScreenToWorldPoint(startPos);
+	}
+	
+	void  OnMouseUp (){
+		endPos= Input.mousePosition;
+		endPos.z = transform.position.z - Camera.main.transform.position.z;
+		endPos = Camera.main.ScreenToWorldPoint(endPos);
 		
+		force= endPos - startPos;
+		force.z = force.magnitude;
+		force.Normalize();
+		
+		rb.AddForce(force * power);
+		//ReturnBall();
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate () {
-		Vector3 sideDir = Vector3.Cross(transform.up, rb.velocity).normalized;
-		rb.AddForce(sideDir * curveAmount);
-	}
-	public void OnPointerDown(BaseEventData  data)
-	{
-		Debug.Log("Begins");
-		PointerEventData e=(PointerEventData) data;
-		swipeBegin=e.position;
-	}
-	
-	public void OnPointerUp(BaseEventData  data)
-	{
-		Debug.Log("Ends");
-		PointerEventData e=(PointerEventData) data;
-		swipeEnd=e.position;
-		detectSwipe();
-	}
-	void detectSwipe()
-	{
-		Debug.Log ("swiped");
-		Vector2 direction = swipeEnd - swipeBegin;
-		Vector3 newDirection = new Vector3 (direction.x, height, direction.y);
-		Vector3 velocity = newDirection * speed;
-		rb.velocity = velocity;
-	}
+	// void ReturnBall (){
+	//yield return new WaitForSeconds(4.0f);
+	//transform.position = Vector3.zero;
+	//rb.velocity = Vector3.zero;
+	//}
 }
