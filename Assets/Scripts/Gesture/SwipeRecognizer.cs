3423c6d340fde5018cba814c9ecf4a426af72220
Dharmaraj P <dharmaraj@skytousolution.com>
2015-06-20 16:43:13 +0530

    Swipe - 8 directions completed
    Todo circle swipe

--- Assets/Scripts/Gesture/SwipeRecognizer.cs
+++ Assets/Scripts/Gesture/SwipeRecognizer.cs
@@ -4,13 +4,27 @@ Description   : Swipe Recognizer
 **/
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic ;
+using System.Collections.Generic;
 
-public class SwipeRecognizer {
+public class SwipeRecognizer
+{
 
 	public enum TouchPattern // Possible gestures
 	{
-		swipeUp, swipeDown, swipeLeft, swipeRight, clockwiseCircle, antiClockwiseCircle, tryAgain
+		swipeUp,
+		swipeDown,
+		swipeLeft,
+		swipeRight,
+
+		swipeUpRight,
+		swipeUpLeft,
+		swipeDownRight,
+		swipeDownLeft,
+
+		clockwiseCircle,
+		antiClockwiseCircle,
+		tryAgain,
+		reset
 	};
 
 	/// <summary>
@@ -20,39 +34,68 @@ public class SwipeRecognizer {
 	/// <param name="startPoint">Start point.</param>
 	/// <param name="endPoint">End point.</param>
 	/// <param name="dragData">Drag data.</param>
-	/// <param name="pattern">Pattern.</param>
+	/// <param name="pattern">Returns predefined TouchPattern if successful else tryAgain .</param>
 	// Called at end of drag to recogonize the gesture
-	public static bool RecogonizeSwipe(Vector2 startPoint, Vector2 endPoint, List<Vector2> dragData, out TouchPattern pattern)
+	public static bool RecogonizeSwipe (Vector2 startPoint, Vector2 endPoint, List<Vector2> dragData, out TouchPattern pattern)
 	{
 		pattern = TouchPattern.tryAgain;
-		float swipeMinDist=2.0f;
+		float swipeMinDist = 10.0f;
 		float swipeDelta;
+		float xComponent;
+		float yComponent;
+		float angle;
 		swipeDelta = startPoint.magnitude - endPoint.magnitude;
-		if (Mathf.Abs (swipeDelta) > swipeMinDist) {
-			// Its a swipe
-			if (swipeDelta > 0) {
-				// Left and Bottom
-				if (Mathf.Abs (startPoint.x - endPoint.x) > Mathf.Abs ((startPoint.y - endPoint.y))) {
-					// Its swipeLeft
-					pattern=TouchPattern.swipeLeft;
-				} 
-				else {
-					// Its swipeDown
-					pattern=TouchPattern.swipeDown;
-				}
-			} 
-			else {
-				// Right and Top
-				if (Mathf.Abs (startPoint.x - endPoint.x) > Mathf.Abs ((startPoint.y - endPoint.y))) {
-					// Its swipeRight
-					pattern=TouchPattern.swipeRight;
-				} 
-				else {
-					// Its swipeUp
-					pattern=TouchPattern.swipeUp;
-				}
+		xComponent= (-startPoint.x + endPoint.x);
+		yComponent= (-startPoint.y + endPoint.y);
+		angle = Mathf.Atan2 (yComponent, xComponent) * Mathf.Rad2Deg; // 0 on right, 180 on left, 90 on top and -90 on bottom
+		//Debug.Log (angle);
+		if(angle>0)
+		{
+			if(angle > 60 && angle < 120)
+			{
+				pattern = TouchPattern.swipeUp;
+			}
+			else if(angle > 30 && angle < 60)
+			{
+				pattern = TouchPattern.swipeUpRight;
+			}
+			else if(angle > 0 && angle < 30)
+			{
+				pattern = TouchPattern.swipeRight;
+			}
+			else if(angle > 120 && angle < 150)
+			{
+				pattern = TouchPattern.swipeUpLeft;
+			}
+			else
+			{
+				pattern = TouchPattern.swipeLeft;
 			}
 		}
+		else
+		{
+			if(angle < -60 && angle > -120)
+			{
+				pattern = TouchPattern.swipeDown;
+			}
+			else if(angle < -30 && angle > -60)
+			{
+				pattern = TouchPattern.swipeDownRight;
+			}
+			else if(angle < 0 && angle > -30)
+			{
+				pattern = TouchPattern.swipeRight;
+			}
+			else if(angle < -120 && angle > -150)
+			{
+				pattern = TouchPattern.swipeDownLeft;
+			}
+			else
+			{
+				pattern = TouchPattern.swipeLeft;
+			}
+		}
+
 		if (pattern == TouchPattern.tryAgain)
 			return false;
 		else
--- Assets/Scripts/Gesture/TouchManager.cs
+++ Assets/Scripts/Gesture/TouchManager.cs
@@ -6,44 +6,111 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TouchManager : MonoBehaviour
 {
+	public Text swipeText;
 	Vector2 startPoint;
 	Vector2 endPoint;
 	List<Vector2> swipeData;
-	int pointerID;
+	int touchBeginID;
+	int touchEndID;
 	SwipeRecognizer.TouchPattern pattern;
 
 	// Use this for initialization
 	void Start ()
 	{
 		swipeData = new List<Vector2> ();	
+		pattern = SwipeRecognizer.TouchPattern.reset;
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		 
+		
+		switch(pattern)
+		{
+		case SwipeRecognizer.TouchPattern.swipeUp:
+			{
+				Debug.Log ("SwipeUp");
+				swipeReset ();
+				break;
+			}
+		case SwipeRecognizer.TouchPattern.swipeDown:
+			{
+				Debug.Log ("SwipeDown");
+				swipeReset ();
+				break;
+			}
+		case SwipeRecognizer.TouchPattern.swipeLeft:
+			{
+				Debug.Log ("SwipeLeft");
+				swipeReset ();
+				break;
+			}
+		case SwipeRecognizer.TouchPattern.swipeRight:
+			{
+				Debug.Log ("SwipeRight");
+				swipeReset ();
+				break;
+			}
+		case SwipeRecognizer.TouchPattern.swipeUpLeft:
+			{
+				Debug.Log ("Diagonal - SwipeUpLeft");
+				swipeReset ();
+				break;
+			}
+		case SwipeRecognizer.TouchPattern.swipeDownLeft:
+			{
+				Debug.Log ("Diagonal - SwipeDownLeft");
+				swipeReset ();
+				break;
+			}
+		case SwipeRecognizer.TouchPattern.swipeUpRight:
+			{
+				Debug.Log ("Diagonal - SwipeUpRight");
+				swipeReset ();
+				break;
+			}
+		case SwipeRecognizer.TouchPattern.swipeDownRight:
+			{
+				Debug.Log ("Diagonal - SwipeDownRight");
+				swipeReset ();
+				break;
+			}
+		}
+
 	}
 
 	public void OnBeginDrag (BaseEventData data)
 	{
+		//Debug.Log ("Begin");
 		PointerEventData pointData = (PointerEventData)data;
-		pointerID = pointData.pointerId;
+		touchBeginID = pointData.pointerId;
 		startPoint = pointData.position;
 		swipeData.Clear ();
 	}
 
 	public void OnEndDrag (BaseEventData data)
 	{
+		//Debug.Log ("End");
 		PointerEventData pointData = (PointerEventData)data;
+		touchEndID = pointData.pointerId;
 		endPoint = pointData.position;
-		SwipeRecognizer.RecogonizeSwipe (startPoint, endPoint, swipeData, out pattern);
+		if(touchBeginID==touchEndID)
+			SwipeRecognizer.RecogonizeSwipe (startPoint, endPoint, swipeData, out pattern);
 	}
 
-	public void OnDrag (BaseEventData Data)
+	public void OnDrag (BaseEventData data)
 	{
+		PointerEventData pointData = (PointerEventData)data;
+		//Debug.Log (pointData.position);
+	}
 
+	void swipeReset()
+	{
+		swipeText.text = pattern.ToString ();
+		pattern = SwipeRecognizer.TouchPattern.reset;
 	}
 }
