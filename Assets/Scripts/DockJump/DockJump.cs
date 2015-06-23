5b1b9aba8a06c6212332e69296ef7497524beef1
Srivatsan <srivatsan@skytousolution.com>
2015-06-23 13:11:05 +0530

    DockJump-workStarted
    inprogress

---
+++ Assets/Scripts/DockJump/DockJump.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DockJump : MonoBehaviour {
+	private Animator dogAnim;
+	public Vector3 jumpHeight;
+	public float moveSpeed=100000000f;
+	bool isJumping=false;
+	public float jumpForce;
+	public float speedDampTime = 0.1f;
+	Rigidbody rb;
+	Vector2 swipeBegin;
+	Vector2 swipeEnd;
+	void awake()
+	{
+		dogAnim = GetComponent<Animator> ();
+		jumpHeight = new Vector3 (0, jumpForce, 0);
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+	void jumping()
+	{
+		dogAnim.SetTrigger ("jump");
+
+	}
+	void running()
+	{
+		dogAnim.SetFloat ("Speed",1f, speedDampTime, Time.deltaTime);
+		rb.AddForce (transform.forward * moveSpeed);
+
+}
+	void OnTriggerStay()
+	{
+		isJumping = true;
+	}
+	
+	void OnTriggerExit()
+	{
+		isJumping = false;
+	}
+	public void OnPointerDown(BaseEventData  data)
+	{
+		Debug.Log("Begins");
+		PointerEventData e=(PointerEventData) data;
+		swipeBegin=e.position;
+	}
+	
+	public void OnPointerUp(BaseEventData  data)
+	{
+		//Debug.Log("Ends");
+		PointerEventData e=(PointerEventData) data;
+		swipeEnd=e.position;
+		detectSwipe();
+	}
+	
+	void detectSwipe()
+	{
+		Vector2 direction=swipeEnd-swipeBegin;
+		direction.Normalize();
+		
+		//swipe upwards
+		if(direction.y > 0 &&  direction.x > -0.5f && direction.x < 0.5f)
+		{
+			Debug.Log("up swipe");
+			if(isJumping)
+			{
+				jumping();
+			}
+				
+			
+		}
+		//swipe down
+		if(direction.y < 0 && direction.x > -0.5f && direction.x < 0.5f)
+		{
+			Debug.Log("down swipe");
+		
+		}
+		//swipe left
+		//if(direction.x < 0 && direction.y > -0.5f && direction.y < 0.5f)
+		//{
+		//	Debug.Log("left swipe");
+		//}
+		//swipe right
+		//if(direction.x > 0 && direction.y > -0.5f && direction.y < 0.5f)
+		//{
+		////	Debug.Log("right swipe");
+		//}
+	}
+}
