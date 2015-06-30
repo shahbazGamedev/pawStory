68287c0617d4fc1fb468fbab8ca7433202cdee59
Srivatsan <srivatsan@skytousolution.com>
2015-06-30 13:27:19 +0530

    ColorLesson- workStarted

---
+++ Assets/Scripts/ColorLesson/DogMovementColorLesson.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogMovementColorLesson : MonoBehaviour {
+	public GameObject Red;
+	public GameObject Blue;
+	public GameObject Green;
+	public GameObject Yellow;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnClick()
+	{
+
+	}
+}
