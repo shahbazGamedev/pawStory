5301b9b187c75c8ca2be7c88f163a0a48c6ce714
Pradeep K <pradeepk@Pradeeps-MacBook-Pro.local>
2015-07-03 16:25:29 +0530

    file and user manager

---
+++ Assets/Scripts/Manager/PuppyWorld_FileManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using com.shephertz.app42.paas.sdk.csharp;
+using com.shephertz.app42.paas.sdk.csharp.social;
+using UnityEngine.SocialPlatforms;
+using AssemblyCSharp;
+
+// Class used to do all the file releated operations. Response was sent to FileManagerResponseCallBack
+
+public class PuppyWorld_FileManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
+
+
+public class PuppyWorld_FileManagerResponseCallBack : App42CallBack
+{
+	
+	public void OnSuccess(object response)  
+	{  
+		
+	}  
+	public void OnException(Exception e)  
+	{  
+		App42Log.Console("Exception : " + e);  
+	}  
+
+
+	public static string response;
+}
---
+++ Assets/Scripts/Manager/PuppyWorld_UserManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using com.shephertz.app42.paas.sdk.csharp;
+using com.shephertz.app42.paas.sdk.csharp.social;
+using UnityEngine.SocialPlatforms;
+using AssemblyCSharp;
+
+// Class to create a user in the server and maintain the data.
+
+public class PuppyWorld_UserManager : MonoBehaviour 
+{
+	PuppyWorld_UserManagerResponseCallBack userResponseCallBack = new PuppyWorld_UserManagerResponseCallBack();
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
+
+public class PuppyWorld_UserManagerResponseCallBack : App42CallBack
+{
+	
+	public void OnSuccess(object response)  
+	{  
+		
+	}  
+	public void OnException(Exception e)  
+	{  
+		App42Log.Console("Exception : " + e);  
+	}  
+}
