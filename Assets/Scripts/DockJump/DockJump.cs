449bed26e366231031b0c4d60f9223965a07dede
Srivatsan <srivatsan@skytousolution.com>
2015-07-15 12:01:56 +0530

    dock jump-UI,jump,animator fix

--- Assets/Scripts/DockJump/DockJump.cs
+++ Assets/Scripts/DockJump/DockJump.cs
@@ -16,6 +16,9 @@ public class DockJump : MonoBehaviour {
 	public float jumpspeed;
 	private float dragRatio;
 	bool isRunning;
+	public int chances =3;
+	private Vector3 dogPos;
+
 	void awake()
 	{
 
@@ -23,39 +26,59 @@ public class DockJump : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		isRunning=false;
 		dogAnim = dogRef.GetComponent<Animator> ();
 		jumpHeight = new Vector3 (0, jumpForce, jumpspeed);
 		rb = GetComponent<Rigidbody> ();
-		isRunning=true;
+	    dogPos=new Vector3(0.16f,1.285f,4.67f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		detectSwipe();
+		movement();
+		//detectSwipe();
+		StartCoroutine(ReturnDog());
+
 
 	}
 	void FixedUpdate() 
 	{
-		if(isRunning)
+
+		if(isRunning==true)
 		{
 		running ();
 		}
 	}
 	void jumping()
 	{
+		isRunning=false; 
 		dogAnim.SetTrigger ("jump");
+		Debug.Log("WORKING FINE");
 		rb.AddForce(jumpHeight,ForceMode.Impulse);
+
+
 
 
 	}
 	void running()
 	{
-		//isRunning=true;
+		//if(isRunning=true)
 		rb.drag = rb.velocity.magnitude * dragRatio;
 		dogAnim.SetFloat ("running",1f, speedDampTime, Time.deltaTime);
 		rb.AddForce (transform.forward * moveSpeed);
 
 }
+
+
+		IEnumerator ReturnDog ()
+		{
+		yield return new WaitForSeconds(7.0f);
+		transform.position = dogPos;
+		rb.velocity = Vector3.zero;
+	
+
+
+	}
 	void OnTriggerStay()
 	{
 		Debug.Log ("triggered");
@@ -66,29 +89,35 @@ public class DockJump : MonoBehaviour {
 	{
 		isJumping = false;
 	}
-	public void OnPointerDown(BaseEventData  data)
-	{
-		Debug.Log("Begins");
-		PointerEventData e=(PointerEventData) data;
-		swipeBegin=e.position;
-	}
-	
-	public void OnPointerUp(BaseEventData  data)
-	{
-		//Debug.Log("Ends");
-		PointerEventData e=(PointerEventData) data;
-		swipeEnd=e.position;
-		detectSwipe();
-	}
-	
-	void detectSwipe()
+//	public void OnPointerDown(BaseEventData  data)
+//	{
+//		Debug.Log("Begins");
+//		PointerEventData e=(PointerEventData) data;
+//		swipeBegin=e.position;
+//	}
+//	
+//	public void OnPointerUp(BaseEventData  data)
+//	{
+//		//Debug.Log("Ends");
+//		PointerEventData e=(PointerEventData) data;
+//		swipeEnd=e.position;
+//		detectSwipe();
+//	}
+	void movement()
 	{
 		if(Input.GetKeyDown(KeyCode.Space)&& isJumping==true)
 		{
 			jumping();
-			isRunning=false; 
-			dogAnim.SetFloat ("running",0f, speedDampTime, Time.deltaTime);
+
+			Debug.Log("WORKING");
+
 		}
+
+	}
+	
+//	void detectSwipe()
+//	{
+
 
 //		Vector2 direction=swipeEnd-swipeBegin;
 //		direction.Normalize();
@@ -121,5 +150,30 @@ public class DockJump : MonoBehaviour {
 		//{
 		////	Debug.Log("right swipe");
 		//}
+
+	//}
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.rigidbody)
+		{
+			chances=chances-1;
+			dogAnim.SetFloat ("running",0f, speedDampTime, Time.deltaTime);
+		}
+	}
+	private void gameStart()
+	{
+		isRunning=true;
+
+	}
+	private void GameOver()
+	{
+		if(chances==0)
+		{
+			//Application.LoadLevel("gameover");
+		}
+	
+
+	
+
 	}
 }
