b80fb5b3b98164b6ba70f57793cd22c396d1b9e8
ponniah <ponniah@skytousolution.com>
2015-08-07 16:44:54 +0530

    Bubble Game

--- Assets/Scripts/Bubble/GlobalValues.cs
+++ Assets/Scripts/Bubble/GlobalValues.cs
@@ -6,26 +6,23 @@ public class GlobalValues : MonoBehaviour {
 	public int baloonsAtScene;
 	public int playerScore;
 
-	void Awake()
-	{
+	void Awake(){
 		Debug.Log ("Hello");
 		DontDestroyOnLoad (this);
 		instanceRef = this;
-
 	}
 
-	// Use this for initialization
+
 	void Start () {
 		Reset ();
 	}
-	
-	// Update is called once per frame
+
+
 	void Update () {
-	
 	}
 
-	public void Reset()
-	{
+
+	public void Reset(){
 		playerScore = 0;
 		baloonsAtScene = 0;
 	}
