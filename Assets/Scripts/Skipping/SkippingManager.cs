e74bf32ddce026037e7e13b7f7522b0eb2b9e64f
Dharmaraj P <dharmaraj@skytousolution.com>
2015-08-10 15:55:23 +0530

    Skipping - Implemented Score & Combos

--- Assets/Scripts/Skipping/SkippingManager.cs
+++ Assets/Scripts/Skipping/SkippingManager.cs
@@ -5,34 +5,74 @@ using System.Collections;
 public class SkippingManager : MonoBehaviour
 {
 
-	public GameObject dogRef;
-	
-	private Animator dogAnimator;
-	private Rigidbody dogRigidBody;
-	
-	// UI components 
+	#region Variables
 
-	public Slider slider;
+	public static SkippingManager instanceRef;
+	public GameObject dogRef;
+	public GameObject[] livesRef;
+
+	public int dogLives;
+	public float scoreIncrement;
+	public int comboMultiplierCap;
 	public float sliderSpeed;
+
+	bool gameStart;
+	bool comboChain;
+	bool lastHit;
+
+	int comboCount;
+	float score; // Always int
+
+	Animator dogAnimator;
+	Rigidbody dogRigidBody;
+
+	// UI components 
+	public Slider slider;
+	public Text scoreText;
+	public Text comboText;
+
+	#endregion
 
 	void Awake()
 	{
 		dogAnimator =  dogRef.GetComponent<Animator>();
-		dogRigidBody = this.GetComponent<Rigidbody>();
+		dogRigidBody = GetComponent<Rigidbody>();
 	}
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		instanceRef = this;
+		gameStart = true;
+		comboCount = 1;
+
+		scoreText.text = "Score: " + score;
+
+		// Add Event Listener
+		TriggerDetection.DogHitSkipRope += DogHitSkipRopeEvent;
+		TriggerDetection.SkipRopeReset += SkipRopeResetEvent;
 	}
 
+	// Update is called once per frame
+	void Update () 
+	{
+		SliderFunction();
+		PlaySkipping();
+	}
+
+	// Decouple Event Listeners
+	void OnDisable()
+	{
+		TriggerDetection.DogHitSkipRope -= DogHitSkipRopeEvent;
+		TriggerDetection.SkipRopeReset -= SkipRopeResetEvent;
+	}
+
+	// Listens for player input // TODO
 	void PlaySkipping()
 	{
 		if(Input.GetMouseButtonDown(0))
 		{
 			dogAnimator.SetTrigger("Skip");
-			 
 		}
 	}
 
@@ -42,11 +82,45 @@ public class SkippingManager : MonoBehaviour
 		slider.value =  Mathf.PingPong(Time.time *sliderSpeed * 2,1);
 	}
 
+	#region EventHandlers
 
-	// Update is called once per frame
-	void Update () 
+	// Event Handler for DogHitSkipRope
+	void DogHitSkipRopeEvent()
 	{
-		SliderFunction();
-		PlaySkipping();
+		dogLives -= 1;
+		comboChain = false;
+		comboCount = 1;
+		lastHit = true;
+		if(dogLives<0)
+		{
+			// GameOver
+		}
 	}
+
+	// Event Handler for SkipRopeReset
+	void SkipRopeResetEvent()
+	{
+		if (!lastHit) 
+		{
+			score += scoreIncrement * comboCount > comboMultiplierCap ? 10 : comboCount;
+			if (comboChain) 
+			{
+				comboCount += 1;
+				comboText.text=comboCount+"X";
+			}
+			else
+			{
+				comboChain = true;
+			}
+		}
+		else
+		{
+			lastHit = false;
+			comboCount = 1;
+			comboText.text="";
+		}
+		scoreText.text = "Score: " + score;
+	}
+
+	#endregion
 }
--- Assets/Scripts/Skipping/TriggerDetection.cs
+++ Assets/Scripts/Skipping/TriggerDetection.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class TriggerDetection : MonoBehaviour {
+	
+	public delegate void DogSkipping();
+	public static event DogSkipping DogHitSkipRope;
+	public static event DogSkipping SkipRopeReset;
 
 	// Use this for initialization
 	void Start () {
@@ -15,9 +19,22 @@ public class TriggerDetection : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.gameObject.tag=="target")
+		if(other.gameObject.tag=="target" && this.gameObject.tag=="Finish")
+		{
+			Debug.Log ("Rope reset");
+			if(SkipRopeReset!=null)
+			{
+				SkipRopeReset ();
+			}
+		}
+		else if(other.gameObject.tag=="target" && this.gameObject.tag=="Player")
 		{
 			Debug.Log ("Hit pass");
+			if(DogHitSkipRope!=null)
+			{
+				DogHitSkipRope ();
+			}
 		}
+
 	}
 }
