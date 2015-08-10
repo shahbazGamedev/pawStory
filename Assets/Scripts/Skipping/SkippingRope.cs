69b41518840fef73e487a52926cb8f2366074810
Dharmaraj P <dharmaraj@skytousolution.com>
2015-08-10 12:16:51 +0530

    Skipping - Implemented Basic GamePlay

---
+++ Assets/Scripts/Skipping/SkippingRope.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkippingRope : MonoBehaviour {
+
+	public float angle;
+	public float skipSpeed;
+
+	// Use this for initialization
+	void Start () {
+		angle = transform.rotation.eulerAngles.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		angle += Time.deltaTime * skipSpeed;
+		transform.rotation=Quaternion.Euler (angle, 0f, 0f);
+	}
+
+}
