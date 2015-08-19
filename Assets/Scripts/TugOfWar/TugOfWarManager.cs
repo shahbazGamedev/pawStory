1f1f91f51cafdb44feae991785202ad3c0f8ad01
Srivatsan <srivatsan@skytousolution.com>
2015-08-19 12:50:52 +0530

    TugOfWar-Code Optimized

--- Assets/Scripts/TugOfWar/TugOfWarManager.cs
+++ Assets/Scripts/TugOfWar/TugOfWarManager.cs
@@ -7,14 +7,23 @@ public class TugOfWarManager : MonoBehaviour {
 	public GameObject gameOver;
 	public GameObject dog;
 	public GameObject pulley;
+	public bool gameStart;
 	public Text winCondition;
+	public Text play;
 	public float speed;
 	private Animator dogAnim;
 	Rigidbody rb;
+	float angle;
+	float preAngle;
+	int value;
+	Vector2 normalizedPositions;
+
+
 
 
 	void Start () 
 	{
+		gameStart=false;
 		win.SetActive(false);
 		gameOver.SetActive(false);
 	    rb=GetComponent<Rigidbody>();
@@ -24,7 +33,29 @@ public class TugOfWarManager : MonoBehaviour {
 
 	void Update ()
 	{
-
+		if(gameStart==true)
+		{
+			normalizedPositions = new Vector2((Input.mousePosition.x/Screen.width-0.5f), ((Input.mousePosition.y/Screen.height)-0.5f));
+			angle = Mathf.Atan2(normalizedPositions.y, normalizedPositions.x)*Mathf.Rad2Deg; 
+			pulley.transform.eulerAngles = new Vector3( 0,0,angle);
+			if(angle<0)
+			{
+				angle += 360;
+				//Debug.Log (angle);
+			}
+			
+			if(angle-preAngle>270)
+			{
+				value+=1;
+				Debug.Log (value);
+				Movement();
+			}
+			else
+			{
+				BackMovement();
+			}
+			preAngle=angle;
+		}
 	}
 
 
@@ -32,17 +63,22 @@ public class TugOfWarManager : MonoBehaviour {
 	{
 
 	}
-
-
-	public void Movement()
+    public void OnPointerDown()
 	{
+		gameStart=true;
+		play.text="";
+	}
+
+	void Movement()
+	{
+		Debug.Log ("MovingForward");
 		rb.AddForce(transform.forward*speed);
 	}
 
 
-	public void BackMovement()
+	 void BackMovement()
 	{
-
+		Debug.Log("MovingBackwards");
 		rb.AddForce(0,0,1*speed);
 	}
 
@@ -73,12 +109,16 @@ public class TugOfWarManager : MonoBehaviour {
 
 	public void ReStart()
 	{
-		Application.LoadLevel("TugOfWar");
+		gameStart=false;
+		value=0;
+		play.text="Tap To Start";
+
+
 	}
 
 
 	public void MainMenu()
 	{
-		Application.LoadLevel("MainMenu");
+		GameMgr.Inst.LoadScene(GlobalConst.Scene_MainMenu);
 	}
 }
