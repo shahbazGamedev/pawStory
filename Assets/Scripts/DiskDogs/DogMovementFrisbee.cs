1d0c9b649a325866b9bc9cd8a04d13fab9398fe9
Srivatsan <srivatsan@skytousolution.com>
2015-08-20 12:53:52 +0530

    DiscDogs-code optimized and some bug fixes

--- Assets/Scripts/DiskDogs/DogMovementFrisbee.cs
+++ Assets/Scripts/DiskDogs/DogMovementFrisbee.cs
@@ -12,10 +12,7 @@ public class DogMovementFrisbee : MonoBehaviour {
 	public Transform Dog;
 	public GameObject dog;
 	public Transform target;
-	public GameObject MenuBtn;
-	public GameObject RestartBtn;
 	public GameObject Frisbee;
-	public GameObject gameOver;
 	public int Score;
 	public GameObject Floor;
 	public bool isMoving;
@@ -56,42 +53,33 @@ public class DogMovementFrisbee : MonoBehaviour {
 		jumpHeight = new Vector3(0 , jumpForce, 0);
 		dogAnim = GetComponent<Animator>();
 		dogPos = new Vector3 (-0.2f, 0.035f, 9.1f);
-		}
+	}
 
 
-	// Use this for initialization
 	void Start ()
 	{
-//		if(isRestart)
-//		{OnRestart();
-//		}
 		EndPanel.SetActive (false);
-
 		flagYellow.SetActive(false);
 		flagGreen.SetActive(false);
 		flagRed.SetActive(false);
 		startPanel.SetActive(true);
 		FrisbeeAttached.SetActive(false);
 		SpawnValueReset();
-		gameOver.SetActive(false);
 		isMoving=true;
-	    MenuBtn.SetActive(false);
-		RestartBtn.SetActive(false);
-		rb = GetComponent<Rigidbody> ();
+	    rb = GetComponent<Rigidbody> ();
 		isGameover=false;
 		Movement();
 		}
 
 
-	// Update is called once per frame
+
 	void Update () 
 	{
 		TempFlag();
 		if(isGameover==true)
 		{
 			GameOver();
-			EndPanel.SetActive(false);
-		}
+        }
 		if(isMoving)
 		{
 		Movement();
@@ -113,17 +101,18 @@ public class DogMovementFrisbee : MonoBehaviour {
 		score.text="Score: "+Score;
 	}
 
+
 	void FixedUpdate()
 	{
-		//movement();
+
 	}
 
 
-public void jumpingRight(Vector3 force)
+    public void jumpingRight(Vector3 force)
 	{
 		dogAnim.SetTrigger("RightJump");
 		rb.velocity=(force);
-		}
+	}
 
 
 	public void jumpingLeft(Vector3 force)
@@ -132,88 +121,6 @@ public void jumpingRight(Vector3 force)
 		rb.velocity = (force);
 	}
 
-
-//	 public IEnumerator ReturnDog ()
-//	{
-//		yield return new WaitForSeconds(2.0f);
-//		transform.position = dogPos;
-//		rb.velocity = Vector3.zero;
-//	}
-//}
-
-
-//	void MovePosition()
-//	{
-//		if(Score==0&& isMoving==true )
-//		{
-//
-//			Debug.Log ("pos1");
-//			Pos1=Vector3.Distance(movePosition1.position,transform.position);
-//			direction1=new Vector3(movePosition1.position.x,0,movePosition1.position.z);
-//			transform.LookAt(direction1);
-//			dogAnim.SetFloat("Walk",1f);
-//			float step=speed*Time.deltaTime;
-//			rb.MovePosition(Vector3.MoveTowards (transform.position, movePosition1.position, step));
-//		
-//		
-//		if(Pos1<1f)
-//			{
-//				isMoving=false;
-//				transform.LookAt(direction);
-//				dogAnim.SetFloat("Walk",0f);
-//
-//			}
-//		}
-//
-//		if(Score==1 && isMoving==true)
-//		{
-//			//isMoving=true;
-//			Debug.Log ("pos2");
-//			Pos2=Vector3.Distance(movePosition2.position,transform.position);
-//			direction2=new Vector3(movePosition2.position.x,0,movePosition2.position.z);
-//			transform.LookAt(direction2);
-//			dogAnim.SetFloat("Walk",1f);
-//			rb.MovePosition(Vector3.MoveTowards (transform.position, movePosition2.position, speed* Time.deltaTime));
-//		
-//			if(Pos2<1f)
-//			{
-//				isMoving=false;
-//				transform.LookAt(direction);
-//				dogAnim.SetFloat("Walk",0f);
-//			}
-//		}
-//		if(Score==2 && isMoving==true)
-//		{
-//
-//			Debug.Log ("pos3");
-//			Pos3=Vector3.Distance(movePosition3.position,transform.position);
-//			direction3=new Vector3(movePosition3.position.x,0,movePosition3.position.z);
-//			transform.LookAt(direction3);
-//			dogAnim.SetFloat("Walk",1f);
-//			rb.MovePosition(Vector3.MoveTowards (transform.position, movePosition3.position, speed* Time.deltaTime));
-//		
-//		if(Pos3<1f)
-//		{
-//			isMoving=false;
-//			transform.LookAt(direction);
-//			dogAnim.SetFloat("Walk",0f);
-//		}
-//	}
-//	}
-	
-
-//	void movement()
-//	{
-//		if(Vector3.Distance(Dog.transform.position,movePosition1.transform.position)>0.2f && Score==0)
-//		{
-//			Debug.Log("not working");
-//			direction=movePosition1.transform.position-Dog.transform.position;
-//			dogAnim.SetFloat("Walk",1f);
-//			transform.LookAt (movePosition1);
-//		//rb.AddForce(transform.forward*Speed);
-//			rb.MovePosition(Vector3.MoveTowards (transform.position, movePosition1.position, speed));
-//		}
-//	}
 
 	public void Movement()
 	{
@@ -241,48 +148,40 @@ public void jumpingRight(Vector3 force)
 		isMoving=false;
 		}
 	}
+
+
 	void FrisebeeCatch()
 	{
 		frisbeedirection = new Vector3 (target.position.x, 0f, target.position.z);
 		if((transform.position-target.position).magnitude>2f)
 			transform.LookAt (frisbeedirection);
 		ChanceUi.text="No Of Chances:"+chances;
-	
-
-
 	}
+
 
 	public void GameOver()
 	{
-		//if(Score>=3)
-		//{
-		    startPanel.SetActive(false);
-			gameOver.SetActive(true);
-			dog.SetActive(false);
-			Frisbee.SetActive(false);
-			RestartBtn.SetActive(true);
-			MenuBtn.SetActive(true);
-			chance.text="SCORE: "+Score;
 
-		//}
-	}
-	public void Restart()
-	{
-
-		Application.LoadLevel("DiscDogs");
+	  startPanel.SetActive(false);
+	  dog.SetActive(false);
+	  Frisbee.SetActive(false);
+	  chance.text="SCORE: "+Score;
+      EndPanel.SetActive(true);
 	}
 
 
 	public void Menu()
 	{
-
-		Application.LoadLevel("MainMenu");
+		GameMgr.Inst.LoadScene(GlobalConst.Scene_MainMenu);
 	}
+
+
 	public void SpawnValueReset()
 	{
 		spawnValue=Random.Range(0,3);
 		targetMove=spawnPoint[spawnValue];
 	}
+
 
 	void TempFlag()
 	{
@@ -306,26 +205,28 @@ public void jumpingRight(Vector3 force)
 		}
 
 	}
-//	public void OnRestart()
-//	{
-//		isMoving=true;
-//		transform.position=dogPos;
-//		Score=0;
-//		chances=0;
-//		MaxChances=10;
-//		Life=3;
-//		isGameover=false;
-//		startPanel.SetActive(true);
-//		dog.SetActive(true);
-//		Frisbee.SetActive(true);
-//		EndPanel.SetActive(false);
-//		isRestart=false;
-//		if(isGameover==true)
-//		{
-//			GameOver();
-//			gameOver.SetActive(true);
-//		}
-//	}
+
+
+	public void OnRestart()
+	{
+		isMoving=true;
+		transform.position=dogPos;
+		Score=0;
+		chances=0;
+		MaxChances=10;
+		Life=3;
+		isGameover=false;
+		startPanel.SetActive(true);
+		dog.SetActive(true);
+		Frisbee.SetActive(true);
+		EndPanel.SetActive(false);
+		isRestart=false;
+		if(isGameover==true)
+		{
+			GameOver();
+
+		}
+	}
 }
 
 
