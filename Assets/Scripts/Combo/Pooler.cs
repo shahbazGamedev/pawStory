a1d56d010c2e592192c7d75a527164c7158c3dc5
Dharmaraj P <dharmaraj@skytousolution.com>
2015-08-27 18:40:39 +0530

    combo  - Added Pooler

--- Assets/Scripts/Combo/Pooler.cs
+++ Assets/Scripts/Combo/Pooler.cs
@@ -1,18 +1,136 @@
 ﻿/**
 Script Author : Vaikash 
-Description   : Generic Object Pooler
+Description   : Object Pooler
 **/
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class Pooler : MonoBehaviour {
+public class Pooler : MonoBehaviour
+{
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
+    #region Var
+
+    public static Pooler InstRef;
+
+    public int poolerObjLimit;
+    public PoolerData[] PoolHolder;
+
+    [System.Serializable]
+    public struct PoolerData
+    {
+        public string name;
+        public int ID; // starts from 0
+        public bool canGrow;
+        //public bool isBusy; // to be implemented if need arises
+        public GameObject prefab;
+        public List<GameObject> pool;
+    }
+
+    #endregion Var
+
+    public void Awake()
+    {
+        InstRef = this;
+    }
+
+    void Start()
+    {
+
+    }
+
+    public void OnEnable()
+    {
+        Init();
+    }
+
+    public void OnDisable()
+    {
+        ShutDown();
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Initialize Pooler
+    void Init()
+    {
+        foreach (var obj in PoolHolder)
+        {
+            PoolInit(obj);
+        }
+    }
+
+    // Clean Pooler
+    void ShutDown()
+    {
+        foreach (var obj in PoolHolder)
+        {
+            PoolKill(obj);
+        }
+    }
+
+    // Create, Store and Hide
+    void PoolInit(PoolerData data)
+    {
+        for (int i = 0; i < poolerObjLimit; i++)
+        {
+            var instance=(GameObject)Instantiate(data.prefab, Vector3.zero, Quaternion.identity);
+            instance.transform.parent = transform;
+            data.pool.Add(instance);
+            instance.SetActive(false);
+        }
+    }
+
+    // Destroy and Clean
+    void PoolKill(PoolerData data)
+    {
+        for (int i = 0; i < data.pool.Count; i++)
+        {
+            Destroy(data.pool[i]);
+        }
+        data.pool.Clear();
+    }
+
+    #region Interface
+
+    // Get a specific type of pooled object
+    public GameObject GetPooledObject(int ID)
+    {
+        GameObject polledInst = null;
+        var pooler=PoolHolder[ID];
+        for (int i = 0; i < pooler.pool.Count; i++)
+        {
+            if(!pooler.pool[i].activeInHierarchy)
+            {
+                polledInst = pooler.pool[i];
+                polledInst.SetActive(true);
+                return polledInst;
+            }
+        }
+        if (pooler.canGrow)
+        {
+            Debug.Log("Growing");
+            var instance=(GameObject)Instantiate(pooler.prefab, Vector3.zero, Quaternion.identity);
+            instance.transform.parent = this.gameObject.transform;
+            pooler.pool.Add(instance);
+            instance.SetActive(true);
+            return instance;
+        }
+        return null;
+    }
+
+    // Pseudo Destroy Code
+    public void Sleep(GameObject Obj)
+    {
+        Obj.transform.position = Vector3.zero;
+        Obj.transform.rotation = Quaternion.identity;
+        Obj.SetActive(false);
+    }
+
+    #endregion Interface
 }
