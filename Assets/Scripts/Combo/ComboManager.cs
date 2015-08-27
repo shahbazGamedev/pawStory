ace05a4b5f4a5458aeacbb4b021fe56645485f90
Dharmaraj P <dharmaraj@skytousolution.com>
2015-08-27 15:26:26 +0530

    Combo - Added Combo Manager and Jump

--- Assets/Scripts/Combo/ComboManager.cs
+++ Assets/Scripts/Combo/ComboManager.cs
@@ -6,13 +6,47 @@ Description   : Game Manager - Jump and Combo
 using UnityEngine;
 using System.Collections;
 
-public class ComboManager : MonoBehaviour {
+public class ComboManager : MonoBehaviour
+{
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
+    // Event Dispatcher
+    public delegate void ComboEventBroadcast();
+    public static ComboEventBroadcast StartGame;
+    public static ComboEventBroadcast Jump;
+
+    public void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        TouchManager.PatternRecognized += HandleSwipeDetection;
+    }
+
+    public void OnDisable()
+    {
+        TouchManager.PatternRecognized -= HandleSwipeDetection;
+    }
+
+    void Update()
+    {
+
+    }
+
+    #region EventHandlers
+
+    // Handle Swipe Detected Event
+    void HandleSwipeDetection(SwipeRecognizer.TouchPattern pattern)
+    {
+        if (pattern == SwipeRecognizer.TouchPattern.swipeUp)
+        {
+            if (Jump != null)
+            {
+                Jump();
+            }
+        }
+    }
+
+    #endregion EventHandlers
 }
--- Assets/Scripts/Combo/DogRunner.cs
+++ Assets/Scripts/Combo/DogRunner.cs
@@ -16,6 +16,8 @@ public class DogRunner : MonoBehaviour
     public float runSpeed;
     public float runDragFactor;
     public Vector3 runDirection;
+    public Vector3 jumpForce;
+    public bool isGrounded;
 
     bool updateAnim;
     float dogVelocity;
@@ -36,11 +38,17 @@ public class DogRunner : MonoBehaviour
     {
         runStart = true;
         updateAnim = true;
+        ComboManager.Jump += HandleDogJump;
+    }
+
+    public void OnDisable()
+    {
+        ComboManager.Jump -= HandleDogJump;
     }
 
     void Update()
     {
-        if(updateAnim)
+        if (updateAnim)
         {
             dogAnim.SetFloat("Speed", dogVelocity);
             updateAnim = false;
@@ -49,16 +57,48 @@ public class DogRunner : MonoBehaviour
 
     public void FixedUpdate()
     {
-        if(runStart)
+        if (runStart)
         {
-           // Moves dog using rigidbody
-           dogRB.AddForce(runDirection * runSpeed * Time.deltaTime);
-           dogRB.drag = dogRB.velocity.magnitude * runDragFactor;
-           dogVelocity = 1;
+            // Moves dog using rigidbody
+            dogRB.AddForce(runDirection * runSpeed * Time.deltaTime);
+            dogRB.drag = dogRB.velocity.magnitude * runDragFactor;
+            dogVelocity = 1;
         }
         else
         {
             dogVelocity = 0;
         }
     }
+
+    public void LateUpdate()
+    {
+        var pos = Camera.main.transform.parent.transform.position;
+        pos.z = dogRB.transform.position.z;
+        Camera.main.transform.parent.transform.position = pos;
+    }
+
+    void OnTriggerStay()
+    {
+        isGrounded = true;
+    }
+
+    void OnTriggerExit()
+    {
+        isGrounded = false;
+    }
+
+    #region EventHandlers
+
+    // Handle Hump Event
+    void HandleDogJump()
+    {
+        if (isGrounded)
+        {
+            dogAnim.SetTrigger("Jump");
+            GetComponent<Rigidbody>().AddForce(jumpForce, ForceMode.Impulse);
+        }
+    }
+
+
+    #endregion EventHandlers
 }
