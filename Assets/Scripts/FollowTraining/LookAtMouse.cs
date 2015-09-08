ad6240d3775ac10417f8f7a0bc435a962b5405f1
Srivatsan <srivatsan@skytousolution.com>
2015-09-08 15:18:48 +0530

    Follow Training-bug fixes and Text added

--- Assets/Scripts/FollowTraining/LookAtMouse.cs
+++ Assets/Scripts/FollowTraining/LookAtMouse.cs
@@ -8,20 +8,29 @@ public class LookAtMouse : MonoBehaviour
 {
 	public float speed;
 	public GameObject headRef;
-	public GameObject dog;
+	bool isPlaying;
 
-	void FixedUpdate () 
-	{
-		Vector3 upAxis = new Vector3(0,0,-1);
-	    Vector3 mouseScreenPosition = Input.mousePosition;
-		mouseScreenPosition.z = transform.position.z;
-		Vector3 mouseWorldSpace = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
-		transform.LookAt(mouseWorldSpace, upAxis);
-		Debug.Log(mouseWorldSpace);
-		transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,0);
-	    if(mouseWorldSpace.y<0)
-		{
-			mouseWorldSpace.y=0;
-		}
-		}
+    void Start()
+    {
+        isPlaying = false;
+    }
+
+    public void OnPointerDown()
+    {
+        isPlaying = true;
+    }
+    void FixedUpdate()
+    {
+        if (isPlaying)
+        {
+            Vector3 upAxis = new Vector3(0, 0, -1);
+            Vector3 mouseScreenPosition = Input.mousePosition;
+            mouseScreenPosition.z = transform.position.z;
+            Vector3 mouseWorldSpace = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+            transform.LookAt(mouseWorldSpace, upAxis);
+            Debug.Log(mouseWorldSpace);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            
+        }
+    }
 	}
