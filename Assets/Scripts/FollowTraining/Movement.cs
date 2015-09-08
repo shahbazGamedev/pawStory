62210d4477051a6a29e3b551fdef73b7ff68f2a6
Srivatsan <srivatsan@skytousolution.com>
2015-09-08 18:35:37 +0530

    NewFollow Training added

---
+++ Assets/Scripts/FollowTraining/Movement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public float distance;
+    public Rigidbody rb;
+    Vector3 pos;
+    public float speed;
+    public Transform target;
+    private Animator dogAnim;
+    public  bool isMoving;
+    
+
+
+    void Start ()
+    {
+        isMoving = true;
+        rb = GetComponent<Rigidbody>();
+        dogAnim = GetComponent<Animator>();
+	}
+	
+	
+	void Update ()
+    {
+        if (isMoving)
+        {
+            DogMovement();
+        }
+	}
+
+    void DogMovement()
+    {
+        distance = Vector3.Distance(target.position, transform.position);
+        if (distance < 6f)
+        {
+                
+                Debug.Log("new");
+                transform.LookAt(target);
+                dogAnim.SetFloat("Walk", 1f);
+                float step = speed * Time.deltaTime;
+                pos = target.position;
+                rb.MovePosition(Vector3.MoveTowards(transform.position, pos, step));
+            }
+            if(distance<2f)
+            {
+                isMoving = false;
+
+                dogAnim.SetFloat("Walk", 0f);
+                
+            }
+        }
+    }
+
---
+++ Assets/Scripts/FollowTraining/NewFollowTraining.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewFollowTraining : MonoBehaviour
+{
+    string layerName;
+    public GameObject Cube;
+    public GameObject Dog;
+    
+  
+ void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 1000f))
+            {
+                Debug.Log("Hit");
+                layerName = LayerMask.LayerToName(hit.collider.gameObject.layer);
+                if (layerName == "Floor")
+                {
+                   Cube.transform.position = hit.point + (Vector3.up * 0.01f);
+                    Dog.GetComponent<Movement>().isMoving = true;
+
+                }
+            }
+        }
+    }
+    public void OnRestart()
+    {
+        Application.LoadLevel("NewFollowTraining");
+    }
+   
+}
