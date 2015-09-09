8f08648f30da395849eaddbd714483949608107f
ponniah-skytou <ponniah@skytousolution.com>
2015-09-09 19:35:16 +0530

    hang up after commit

--- Assets/Scripts/International/InternationalTournament.cs
+++ Assets/Scripts/International/InternationalTournament.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class InternationalTournament : MonoBehaviour {
 
     int dogAnimState = 0; // 0: Idle, 1: Idle 2, 2: Idle 3, 3: Run 
     public Animator DogAnimator;
     private bool waitForTap = true;
+    Vector3 dogMoveOffset = Vector3.zero;
+    float dogSpeed = 10;
+    public List<GameObject> Nodes;
+    int curNode = 0;
 
     void OnEnable()
     {
@@ -36,6 +40,7 @@ public class InternationalTournament : MonoBehaviour {
                     waitForTap = false;
                     dogAnimState = 3;
                     DogAnimator.SetInteger("DogAnimState", dogAnimState);
+                    curNode = 0;
                 }
             }
 #endif
@@ -44,12 +49,33 @@ public class InternationalTournament : MonoBehaviour {
                 waitForTap = false;
                 dogAnimState = 3;
                 DogAnimator.SetInteger("DogAnimState", dogAnimState);
+                curNode = 0;
             }
         }
         else
         {
-                
+            // move the dog
+            transform.LookAt(Nodes[curNode].transform, new Vector3(0, 1, 0));   
+            transform.position = Vector3.MoveTowards(transform.position, Nodes[curNode].transform.position, Time.deltaTime * dogSpeed);
+            
+            if (Vector3.Distance(transform.position, Nodes[curNode].transform.position) < 0.1f)
+            {
+                curNode++;
+                if (curNode >= Nodes.Count)
+                {
+                    curNode = 0;
+                }
+            }
+            //transform.position += dogMoveOffset;
         }
+	}
+
+
+	private void LateUpdate()
+	{
+		if (!waitForTap) {
+			//Camera.main.transform.position += dogMoveOffset;
+		}
 	}
 
 
