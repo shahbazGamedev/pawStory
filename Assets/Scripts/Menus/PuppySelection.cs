06da74762a4460a9b9aa8c008bd32ae82002ce64
Srivatsan <srivatsan@skytousolution.com>
2015-09-09 16:53:08 +0530

    puppy selection-Camera movement added

--- Assets/Scripts/Menus/PuppySelection.cs
+++ Assets/Scripts/Menus/PuppySelection.cs
@@ -2,20 +2,60 @@
 using System.Collections;
 
 public class PuppySelection : MonoBehaviour {
+    Vector3 startingPos;
+    Vector3 endPos;
+    bool backMove;
+    bool FrontMove;
+    
 
-
-	void Start () {
-	
+	void Start ()
+    {
+        startingPos = Camera.main.transform.position;
+        endPos = new Vector3(0.37f, 1.85f, 10.05f);
+        FrontMove = true;
+        
 	}
 	
 
-	void Update () {
-	
+	void Update ()
+    {
+        if(Camera.main.transform.position==startingPos)
+        {
+            backMove = false;
+        }
+        else
+        {
+            backMove = true;
+        }
+        if(Camera.main.transform.position == endPos)
+        {
+            FrontMove = false;
+        }
+        else
+        {
+            FrontMove = true;
+        }
+
 	}
 
 
     public void OnSelect()
     {
         GameMgr.Inst.LoadScene(GlobalConst.Scene_MainMenu);
+    }
+    public void Transition()
+    {
+        if (FrontMove)
+        {
+            Camera.main.transform.position += new Vector3(6, 0, 0);
+        }
+
+    }
+    public void BackTransition()
+    {
+        if (backMove)
+        {
+            Camera.main.transform.position += new Vector3(-6, 0, 0);
+        }
     }
 }
