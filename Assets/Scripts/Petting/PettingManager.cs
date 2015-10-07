2b97f0e06d312003341e3df264881397bb00d517
Dharmaraj P <dharmaraj@skytousolution.com>
2015-10-07 16:13:32 +0530

    Petting - Added Scene & Scripts

---
+++ Assets/Scripts/Petting/PettingManager.cs
@@ -0,0 +1,20 @@
+﻿/**
+Script Author : Vaikash
+Description   : Game Manager for Petting
+**/
+
+using UnityEngine;
+using System.Collections;
+
+public class PettingManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
