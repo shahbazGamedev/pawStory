170583e85d7e9d04016485923e4d444888c16a47
Srivatsan <srivatsan@skytousolution.com>
2015-10-12 18:08:33 +0530

    ColorLesson-Repeated Click Fix

--- Assets/Scripts/ColorLesson/DogMovementColorLesson.cs
+++ Assets/Scripts/ColorLesson/DogMovementColorLesson.cs
@@ -18,22 +18,29 @@ public class DogMovementColorLesson : MonoBehaviour {
 	public Transform yellow;
     public float distance;
     public GameObject PanelGameOver;
-   
-	
+    string layerName;
+    bool collectRed;
+    public bool collectBlue;
+    bool collectGreen;
+    bool collectWhite;
+    bool canClick;
 
 
-	
-	void Start ()
+
+
+
+    void Start ()
     {
 		rb=GetComponent<Rigidbody>();
         isMoving = false;
 		dogAnim = GetComponent<Animator>();
         PanelGameOver.SetActive(false);
-        
+        canClick = true;
 
 
-	
-	}
+
+
+    }
 	
 
 	void Update ()
@@ -50,45 +57,87 @@ public class DogMovementColorLesson : MonoBehaviour {
         if (distance < 2f)
         {
             isMoving = false;
+            canClick = true;
 
             dogAnim.SetFloat("Walk", 0f);
 
         }
-
+        
     }
 
 
-	void FixedUpdate()
+    void TargetCreator()
+    {
+        Debug.Log("yes yes yes");
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 1000f))
+            {
+                Debug.Log("Hit");
+                layerName = LayerMask.LayerToName(hit.collider.gameObject.layer);
+                if (layerName == "Floor")
+                {
+                    collectBlue=true;
+                    Debug.Log("steve");
+                    
+                    //isMoving = true;
+
+                }
+            }
+        }
+    }
+
+    void FixedUpdate()
 	{
     }
        
 
     public void RedMove()
 	{
-		Target=red;
-        isMoving = true;
+        if (canClick)
+        {
+            canClick = false;
+            Target = red;
+            isMoving = true;
+        }
         
 	}
 
 
-	public void BlueMove()
-	{
-		Target=blue;
-        isMoving = true;
-	}
+    public void BlueMove()
+    {
+        if (canClick)
+        {
+
+            canClick = false;
+            Target = blue;
+            isMoving = true;
+        }
+    }
 
 
 	public void GreenMove()
 	{
-		Target=green;
-        isMoving = true;
+        if (canClick)
+        {
+
+            canClick = false;
+            Target = green;
+            isMoving = true;
+        }
 	}
 
 
 	public void YellowMove()
 	{
-	    Target=yellow;
-        isMoving = true;
+        if (canClick)
+        {
+            canClick = false;
+            Target = yellow;
+            isMoving = true;
+        }
 	}
 
 
