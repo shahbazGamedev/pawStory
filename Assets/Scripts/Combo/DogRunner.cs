88cca3e9fbbe93abc44b3751c93f03d6c0041fe1
Dharmaraj P <dharmaraj@skytousolution.com>
2015-10-17 16:04:18 +0530

    Combo - Reworked Character Movement

--- Assets/Scripts/Combo/DogRunner.cs
+++ Assets/Scripts/Combo/DogRunner.cs
@@ -14,26 +14,39 @@ public class DogRunner : MonoBehaviour
 
     public bool runStart;
     public float runSpeed;
-    public float maxSpeed;
-    public float runDragFactor;
+    //public float runDragFactor;
     public Vector3 runDirection;
-    public Vector3 jumpForce;
+    //public Vector3 jumpForce;
     public bool isGrounded;
+
 
     public bool updateAnim;
     Vector3 startPos;
     float dogVelocity;
+    bool jump;
     bool gameOver;
+    float time;
 
-    Rigidbody dogRB;
+    CharacterController dogCC;
     public Animator dogAnim;
+
+    public float jumpSpeed;
+    //Vector3 direction = Vector3.zero;
+    float verticalVelocity = 0f;
+
+    // Camera 
+    public float smoothDampTime;
+    public Vector3 cameraOffset;
+    Vector3 smoothDampVelocity;
+
+    public float springForce;
 
     #endregion Variables
 
     public void Awake()
     {
         instRef = this;
-        dogRB = GetComponent<Rigidbody>();
+        dogCC = GetComponent<CharacterController>();
         dogAnim = GetComponent<Animator>();
     }
 
@@ -57,38 +70,67 @@ public class DogRunner : MonoBehaviour
         CamUp();
     }
 
-    public void FixedUpdate()
+    public void Update()
     {
+       
         if (runStart)
         {
-            // Moves dog using rigidbody - need to change
-            dogRB.AddForce(runDirection * runSpeed * Time.deltaTime);
-            //Debug.Log(dogRB.velocity.magnitude);
-            //if (dogRB.velocity.magnitude > maxSpeed)
-            //{
-            //    dogRB.velocity = dogRB.velocity.normalized * maxSpeed;
-            //}
-            dogRB.drag = dogRB.velocity.magnitude * runDragFactor;
+            // Handle Movement
+            Vector3 dist = runDirection * runSpeed * Time.deltaTime;
+            if (dogCC.isGrounded && verticalVelocity < 0)
+            {
+                verticalVelocity = Physics.gravity.y * Time.deltaTime;
+            }
+            else
+            {
+                verticalVelocity += 2.5f * Physics.gravity.y * Time.deltaTime;
+            }
+
+            // Handle Jump
+            if (dogCC.isGrounded && jump)
+            {
+                Debug.Log(verticalVelocity);
+                StartCoroutine(JumpForce(jumpSpeed));
+                jump = false;
+            }
+
+            dist.y = verticalVelocity * Time.deltaTime;
+            dogCC.Move(dist);
+
             dogVelocity = 1;
             SyncAnim();
+            //dogCC.SimpleMove(runDirection * runSpeed * Time.deltaTime);
+
         }
-        if (dogRB.velocity.magnitude <= 0.1f && !gameOver)
-        {
-            gameOver = true;
-            ComboManager.instRef.GameOver();
-            runStart = false;
-            dogVelocity = 0;
-            updateAnim = true;
-            SyncAnim();
-        }
+
     }
+
+    //public void FixedUpdate()
+    //{
+        //if (runStart)
+        //{
+        //    //Moves dog using rigidbody - need to change
+        //    dogRB.AddForce(runDirection * runSpeed * Time.deltaTime);
+        //    Debug.Log(Vector3.Distance(dogRB.position, prevPos));
+        //    //if (dogRB.velocity.magnitude > maxSpeed)
+        //    //{
+        //    //    dogRB.velocity = dogRB.velocity.normalized * maxSpeed;
+        //    //}
+        //    dogRB.drag = dogRB.velocity.magnitude * runDragFactor;
+        //    //dogRB.MovePosition(dogRB.position + runDirection * runSpeed * Time.deltaTime);
+        //    //prevPos = dogRB.position;
+        //    dogVelocity = 1;
+        //    SyncAnim();
+        //}
+    //}
 
     // Camera Movement
     void CamUp()
     {
         var pos = Camera.main.transform.parent.transform.position;
-        pos.z = dogRB.transform.position.z;
-        Camera.main.transform.parent.transform.position = pos;
+        pos.z = transform.position.z;
+        // Camera.main.transform.parent.transform.position = pos;
+        Camera.main.transform.parent.transform.position = Vector3.SmoothDamp(Camera.main.transform.parent.transform.position, pos - cameraOffset, ref smoothDampVelocity, smoothDampTime); ;
     }
 
     // Reset GameOver Flag
@@ -105,6 +147,19 @@ public class DogRunner : MonoBehaviour
             dogAnim.SetFloat("Speed", dogVelocity);
             updateAnim = false;
         }
+    }
+
+    // GameOver
+    public void GameOver()
+    {
+
+        gameOver = true;
+        ComboManager.instRef.GameOver();
+        runStart = false;
+        dogVelocity = 0;
+        updateAnim = true;
+        SyncAnim();
+
     }
 
     // Reset Dog Pos
@@ -128,11 +183,17 @@ public class DogRunner : MonoBehaviour
     // Handle Jump Event
     void HandleDogJump()
     {
-        if (isGrounded && runStart)
+
+        if(dogCC.isGrounded && runStart)
         {
+            jump = true;
             dogAnim.SetTrigger("Jump");
-            GetComponent<Rigidbody>().AddForce(jumpForce, ForceMode.Impulse);
         }
+        //if (isGrounded && runStart)
+        //{
+        //    dogAnim.SetTrigger("Jump");
+        //    GetComponent<CharacterController>().AddForce(jumpForce, ForceMode.Impulse);
+        //}
     }
 
     // Game start event handler
@@ -143,4 +204,40 @@ public class DogRunner : MonoBehaviour
         Invoke("ResetGameOverFlag", 2f);
     }
     #endregion EventHandlers
+
+    #region Coroutines
+
+    IEnumerator JumpForce(float force)
+    {
+        while(time<0.2f)
+        {
+            verticalVelocity += force * Time.deltaTime;
+            time += Time.deltaTime;
+            force -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        time = 0;
+        yield return null;
+    }
+
+
+
+    #endregion Coroutines
+
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null)
+            return;
+        if (body.tag == "target")
+        {
+            dogAnim.SetTrigger("Fly");
+            StartCoroutine(JumpForce(springForce));
+        }
+        else if(body.tag == "LoseLine")
+        {
+            GameOver();
+        }
+    }
 }
