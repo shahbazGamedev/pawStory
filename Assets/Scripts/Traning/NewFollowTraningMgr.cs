368cc9ff30b63cd5d54686a068deb010304193d2
Srivatsan <srivatsan@skytousolution.com>
2015-10-22 16:52:00 +0530

    Follow Training
    Timer,Win&Lose Condition also Target hit Implemented

--- Assets/Scripts/Traning/NewFollowTraningMgr.cs
+++ Assets/Scripts/Traning/NewFollowTraningMgr.cs
@@ -1,22 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class NewFollowTraningMgr : MonoBehaviour
 {
     public List<GameObject> Nodes;
     public GameObject dog;
     public GameObject gameOverPanel;
+    public GameObject failPanel;
     int curNode = -1;
     public bool isMoving;
-    bool canTap;
+    public bool canTap;
     public float dogSpeed;
     Animator dogAnim;
-    bool gotoStart;
     public Vector3 StartPos;
-    public GameObject canvas;
-    public GameObject enemyCar;
-    GameObject createImage;
+    public GameObject movingTarget;
+    public float timer;
+    public Text TxtTimer;
+    public Text TxtGameOver;
+    
+    
    
 
 
@@ -25,11 +29,11 @@ public class NewFollowTraningMgr : MonoBehaviour
     void Start()
     {
         dogAnim = GetComponent<Animator>();
-        canTap = true;
         gameOverPanel.SetActive(false);
         StartPos = transform.position;
         OnRestartGame();
-        CreateEnemy();
+       // CreateEnemy();
+        
 
 
     }
@@ -53,13 +57,29 @@ public class NewFollowTraningMgr : MonoBehaviour
         {
             Movement();
         }
-        Debug.Log(createImage.transform.position.x);
-        createImage.transform.Translate(100*Time.deltaTime, 0, 0);
-        if(createImage.transform.position.x >400f)
+        movingTarget.transform.Translate(250 * Time.deltaTime, 0, 0);
+        Debug.Log(movingTarget.transform.position.x);
+        if(movingTarget.transform.position.x >= 1000)
+        {
+            movingTarget.transform.position = new Vector2(0, 0);
+        }
+        if(movingTarget.transform.position.x>485 && movingTarget.transform.position.x<500)
         {
             Debug.Log("yes");
-            createImage.transform.position = new Vector2(0, 20f);
+            canTap = true;
         }
+        else
+        {
+            canTap = false;
+        }
+      
+    }
+
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        TxtTimer.text = "Time :" +(int)timer;
+        GameOver();
     }
 
 
@@ -73,7 +93,7 @@ public class NewFollowTraningMgr : MonoBehaviour
         {
             dogAnim.SetFloat("Walk", 0f);
             isMoving = false;
-            canTap = true;
+            
 
         }
     }
@@ -86,6 +106,7 @@ public class NewFollowTraningMgr : MonoBehaviour
             
             Debug.Log("collided");
             gameOverPanel.SetActive(true);
+            TxtGameOver.text = "Follow Traning Sucessful!!";
             
             
         }
@@ -99,7 +120,7 @@ public class NewFollowTraningMgr : MonoBehaviour
             curNode++;
             isMoving = true;
             Debug.Log(curNode);
-            canTap = false;
+            
 
 
         }
@@ -114,20 +135,25 @@ public class NewFollowTraningMgr : MonoBehaviour
         gameOverPanel.SetActive(false);
         isMoving = false;
         dogAnim.SetFloat("Walk", 0f);
-        canTap = true;
         curNode = -1;
+        movingTarget.SetActive(true);
+        movingTarget.transform.position = new Vector2(0, 0);
+        timer = 0f;
+
     }
     
 
-    void CreateEnemy()
+  
+    void GameOver()
     {
-        
-            createImage = Instantiate(enemyCar) as GameObject;
-            createImage.transform.SetParent(canvas.transform, false);
-            
+        if(timer >= 45)
+        {
+            gameOverPanel.SetActive(true);
+            TxtGameOver.text = "Traning Session Failed!!!";
+            movingTarget.SetActive(false);
 
+        }
     }
-
 
 
 
