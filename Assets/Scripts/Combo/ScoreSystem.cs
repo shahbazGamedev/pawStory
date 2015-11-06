8321c7bfca0c2682eeb0ea8c2be42049721ecda6
Dharmaraj P <dharmaraj@skytousolution.com>
2015-11-06 15:25:33 +0530

    Combo - Added Score System

---
+++ Assets/Scripts/Combo/ScoreSystem.cs
@@ -0,0 +1,55 @@
+﻿/**
+Script Author : Vaikash 
+Description   : Score Manager - Jump and Combo
+**/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+
+    public static ScoreSystem instRef;
+
+    public int scoreIncrement;
+    public int maxComboAllowed;
+
+    // UI 
+    public Text scoreDisp;
+
+    int score;
+    int comboCount;
+
+    public void Awake()
+    {
+        instRef = this;
+    }
+
+    // Increase score
+    void UpdateScore()
+    {
+        score += scoreIncrement * comboCount;
+    }
+
+    // Increase combo count
+    public void UpdateCombo()
+    {
+        comboCount += 1;
+        comboCount = comboCount > maxComboAllowed ? maxComboAllowed : comboCount;
+        UpdateScore();
+    }
+
+    // Reset combo count
+    public void ComboBroken()
+    {
+        comboCount = 0;
+    }
+
+    // update Score Display
+    void UpdateScoreUI()
+    {
+
+    }
+
+}
