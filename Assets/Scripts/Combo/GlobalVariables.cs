5670b7d39f4ffbb12e7f1052be350436064da247
harishskytou <harish@skytousolution.com>
2016-03-16 11:29:21 +0530

    Global Variables Added

---
+++ Assets/Scripts/Combo/GlobalVariables.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalVariables : MonoBehaviour
+{
+
+	public static int numberOfRetry;
+	public static float distanceCovered;
+	public static long score;
+
+	public static bool isMuted =false;
+
+}
