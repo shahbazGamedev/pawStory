1d603b9699fb1fe47935027b3cca31823b36e42a
Praveen <praveen@skytousolution.com>
2016-03-28 17:07:13 +0530

    Optimized the bone pulling script.

--- Assets/Scripts/BonePullManager.cs
+++ Assets/Scripts/BonePullManager.cs
@@ -18,7 +18,7 @@ public class BonePullManager : MonoBehaviour {
     //rotation vars
     private float startTime_Rotation, startTime_SliderMove;
     private float distanceCovered;
-    private float rotationSpeed=18f;
+    private float rotationSpeed;
     private float currentDogAngle;
     private float maxDogRotationAngle = 35f;
 
@@ -30,6 +30,8 @@ public class BonePullManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
         animator = dog.GetComponent<Animator>();
+
+        rotationSpeed = maxDogRotationAngle / 3f;
     }
 	
 	// Update is called once per frame
@@ -37,8 +39,9 @@ public class BonePullManager : MonoBehaviour {
 
         if (!hasCurrentTurnEnded)
         {
-			rotateToRight();
-            moveSliderLeft();
+            //rotateToRight();
+            // moveSliderLeft();
+            moveSliderRight();
         }
 
         //bone pulling animation
@@ -61,72 +64,101 @@ public class BonePullManager : MonoBehaviour {
     void OnMouseDown()
     {
 
-        if (hasCurrentTurnEnded && !isDogRotating)
+        //if (hasCurrentTurnEnded && !isDogRotating)
+        //{
+        //    hasCurrentTurnEnded = false;
+        //    isDogRotating = true;
+        //    startTime_Rotation = Time.time;
+        //    startTime_SliderMove = Time.time;
+        //    Debug.Log("started rotating");
+        //}
+
+
+        if (hasCurrentTurnEnded)
         {
             hasCurrentTurnEnded = false;
-            isDogRotating = true;
-            startTime_Rotation = Time.time;
+            //startTime_Rotation = Time.time;
             startTime_SliderMove = Time.time;
             Debug.Log("started rotating");
         }
 
     }
 
-    void OnMouseUp()
-    {
 
-    }
-	
-	void rotateToRight()
-	{
+	//void rotateToRight()
+	//{
 
-        if (isDogRotating)
-        {
-            distanceCovered = (Time.time - startTime_Rotation) * rotationSpeed;
-            //Debug.Log("distance covered = " + distanceCovered + " , distDiff = " + distanceCovered / 30f);
-            currentDogAngle = Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(0, maxDogRotationAngle, 0), distanceCovered / maxDogRotationAngle).y;
-            this.transform.rotation = Quaternion.Euler(0, currentDogAngle, 0);
+ //       if (isDogRotating)
+ //       {
+ //           distanceCovered = (Time.time - startTime_Rotation) * rotationSpeed;
+ //           //Debug.Log("distance covered = " + distanceCovered + " , distDiff = " + distanceCovered / 30f);
+ //           currentDogAngle = Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(0, maxDogRotationAngle, 0), distanceCovered / maxDogRotationAngle).y;
+ //           this.transform.rotation = Quaternion.Euler(0, currentDogAngle, 0);
 
-            if (currentDogAngle == maxDogRotationAngle)
-                isDogRotating = false;
-        }
-    }
+ //           if (currentDogAngle == maxDogRotationAngle)
+ //               isDogRotating = false;
+ //       }
+ //   }
 
-    void rotateToLeft()
-    {
-        if (isDogRotating)
-        {
-            distanceCovered = (Time.time - startTime_Rotation) * rotationSpeed;
-            //Debug.Log("distance covered = " + distanceCovered + " , distDiff = " + distanceCovered / 30f);
-            currentDogAngle = Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(0, -maxDogRotationAngle, 0), distanceCovered / maxDogRotationAngle).y;
-            this.transform.rotation = Quaternion.Euler(0, currentDogAngle, 0);
+ //   void rotateToLeft()
+ //   {
+ //       if (isDogRotating)
+ //       {
+ //           distanceCovered = (Time.time - startTime_Rotation) * rotationSpeed;
+ //           //Debug.Log("distance covered = " + distanceCovered + " , distDiff = " + distanceCovered / 30f);
+ //           currentDogAngle = Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(0, -maxDogRotationAngle, 0), distanceCovered / maxDogRotationAngle).y;
+ //           this.transform.rotation = Quaternion.Euler(0, currentDogAngle, 0);
 
-            if (currentDogAngle == -maxDogRotationAngle)
-                isDogRotating = false;
-        }
-    }
+ //           if (currentDogAngle == -maxDogRotationAngle)
+ //               isDogRotating = false;
+ //       }
+ //   }
 
-    //Movement from center to left
+    //Movement from center to left (0.5 to 0)
     void moveSliderLeft()
     {
 
         if (!hasSliderReachedEnd)
         {
-            slider.value = 0.5f - sliderSpeed * (Time.time - startTime_SliderMove);
+            slider.value = 0.5f - ( sliderSpeed * (Time.time - startTime_SliderMove) );
+            currentDogAngle = (0.5f - slider.value) * (maxDogRotationAngle / 0.5f);
+            this.transform.rotation = Quaternion.Euler(0, currentDogAngle, 0);//dog rotates towards right(0 to 30)
+
             if (slider.value == 0)
                 hasSliderReachedEnd = true;
         }
+        else
+        {
+            //slider(0 to 0.5)
+            currentDogAngle = (0.5f - slider.value) * (maxDogRotationAngle / 0.5f);
+
+            this.transform.rotation = Quaternion.Euler(0, -currentDogAngle, 0);//dog rotates towards left(30 to 0)
+
+        }        
     }
 
-    //Movement from center to right
+    //Movement from center to right  (0.5 to 1)
     void moveSliderRight()
     {
 
         if (!hasSliderReachedEnd)
         {
-            slider.value = 0.5f + sliderSpeed * (Time.time - startTime_SliderMove);
+            slider.value = 0.5f + ( sliderSpeed * (Time.time - startTime_SliderMove) );
+            currentDogAngle = (slider.value - 0.5f) * (maxDogRotationAngle / 0.5f);
+
+            this.transform.rotation = Quaternion.Euler(0, -currentDogAngle, 0);//dog rotates towards left(0 to -30)
+
             if (slider.value == 1)
                 hasSliderReachedEnd = true;
         }
+        else
+        {
+            //slider(1 to 0.5)
+            currentDogAngle = (slider.value - 0.5f) * (maxDogRotationAngle / 0.5f);
+
+            this.transform.rotation = Quaternion.Euler(0, -currentDogAngle, 0);//dog rotates towards right(0 to -30)
+
+        }
     }
+
 }
